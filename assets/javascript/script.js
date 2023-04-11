b8bdbcd7aece8bb07fb3e6149047cf3bbc3ccb80
ESTEVAN DADDY <enunez391@gmail.com>
2023-04-11 11:05:46 -0700

    added each character type in their own array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,3 +2,8 @@
 var charLength;
 //this sets a global variable for array below
 var choiceArr = [];
+
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
+var lowerCase =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
